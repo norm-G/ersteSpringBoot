f9ef5240be5e1947f8b5d45d9c1b3b05c66d620f
ngersdorf <n.gersdorf@googlemail.com>
2020-05-14 13:23:06 +0200

    Erste Tests

--- ersteSpringBoot/src/main/java/ng/uebungen/erste/controller/HalloController.java
+++ ersteSpringBoot/src/main/java/ng/uebungen/erste/controller/HalloController.java
@@ -3,6 +3,8 @@ package ng.uebungen.erste.controller;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,16 +17,16 @@ public class HalloController {
 	 * Uebersicht
 	 */
 	@GetMapping("/")
-	public EntityModel<String> sayHello() {
+	public ResponseEntity<?> sayHello() {
 		
+		EntityModel<String> hallo =  new EntityModel<>("Hallo World",
+														linkTo(HalloController.class).withSelfRel(),
+														linkTo(NutzerController.class).withRel("nutzer"),
+														linkTo(RollenController.class).withRel("rollen"),
+														linkTo(EinkaufController.class).withRel("einkauf"),
+														linkTo(ArtikelController.class).withRel("artikel"));
 			
-		return new EntityModel<>("Hallo World",
-									linkTo(HalloController.class).withSelfRel(),
-									linkTo(NutzerController.class).withRel("nutzer"),
-									linkTo(RollenController.class).withRel("rollen"),
-									linkTo(EinkaufController.class).withRel("einkauf"),
-									linkTo(ArtikelController.class).withRel("artikel")
-								);
+		return ResponseEntity.status(HttpStatus.OK).body(hallo);
 	}
 	
 }
